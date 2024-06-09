da29fef8fad21f5c3ea0e82ea03991ef30ef7f31
firstquarterlee <firstquarterlee@gmail.com>
2024-06-10 01:50:12 +0900

    06/10 공부

--- dart_basic/lib/main.dart
+++ dart_basic/lib/main.dart
@@ -3,6 +3,48 @@ import 'package:flutter/material.dart';
 void main() {
   ///정수
   ///Integer
-  int number1 = 10;
-  print(number1);
+  // int number1 = 10;
+  // print(number1);
+  // int number1 = 2;
+  // int number2 = 4;
+  // print(number1 + number2);
+  ///======================
+  ///실수
+  ///Double
+  // double number1 = 2.5;
+  // double number2 = 0.5;
+  // print(number1 + number2);
+  ///======================
+  ///맞다 / 틀린다
+  ///Boolean
+  // bool isTrue = true;
+  // bool isFalse = false;
+  // print(isTrue);
+  ///======================
+  ///글자 타입
+  ///String
+  // String name = '레드벨벳';
+  // String name2 = '코드';
+  // print(name);
+  // print(name2)
+  ///======================
+  ///var String
+  // var name3 = '블랙핑크'; //-> 이러면 자동으로 오른쪽 값을 보고 타입을 유추했음
+  // var number = 20;
+  // print(name3.runtimeType); //타입알려줌
+  //직접 String 이렇게 명시해줘야 코드가 몇만줄이 넘으면 가독성이 좋음
+  //var name3 = '블랙핑크"; ->이렇게하면 유추해야되서 가독성이 떨어짐
+  ///======================
+  ///글자 덧셈
+  // String name = '레드벨벳';
+  // String name2 = '슬기';
+  // print(name + name2);
+  // print(name + ' ' + name2);
+  // //'${}' 이렇게도 변수를 넣을 수 있음
+  // print('${name} ${name2}');
+  // print('${name.runtimeType} ${name2}'); //이건 함수까지 가능
+  // //'$ 이것도 가능
+  // print('$name $name2');
+  // print('$name.runtimeType $name2'); //이건 함수 불가능
+
 }
