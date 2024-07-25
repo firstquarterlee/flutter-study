8a5796d5c5eb80181ca116e1834fc998eb7296ca
이상현 <firstquarterlee@gmail.com>
2024-07-26 01:16:08 +0900

    init

---
+++ dusty_dust/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:dusty_dust/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'sunflower',
+      ),
+      home: HomeScreen(),
+    ),
+  );
+}
---
+++ dusty_dust/lib/screen/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:dusty_dust/component/category_stat.dart';
+import 'package:dusty_dust/component/main_stat.dart';
+import 'package:dusty_dust/const/color.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            MainStat(),
+            CategoryStat(),
+          ],
+        ),
+      ),
+    );
+  }
+}
